a9c9686b718892806ae415533ca5b334940567c0
powell956 <powell956@gmail.com>
2018-12-05 11:19:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,37 @@
-# Write your code here.
+def takeANumber(katz_deli_line, name)
+  var place = katz_deli_line.length + 1
+  katz_deli_line.push(name)
+  return 'Welcome, ' + name + '. You are number ' + place + ' in line.'
+
+function takeANumber(katzDeliLine, name) {
+  var place = katzDeliLine.length + 1;
+  katzDeliLine.push(name);
+  return 'Welcome, ' + name + '. You are number ' + place + ' in line.';
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return 'There is nobody waiting to be served!';
+  }
+
+  var next = katzDeliLine.shift();
+  return 'Currently serving ' + next + '.';
+}
+
+function currentLine(katzDeliLine) {
+  var people = '';
+
+  if (katzDeliLine.length === 0) {
+    return 'The line is currently empty.';
+  }
+
+  for (var i = 0; i < katzDeliLine.length; i++) {
+    if (people === '') {
+      people += '1. ' + katzDeliLine[i];
+    } else {
+      people += ', ' + (i + 1) + '. ' + katzDeliLine[i];
+    }
+  }
+
+  return 'The line is currently: ' + people;
+}
